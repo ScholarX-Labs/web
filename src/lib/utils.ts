478ed7cbf10b1bed69b92ddaac9929453fd46535
marwan051 <marwanmohamed045@gmail.com>
2026-04-08 18:27:12 +0200

    refactor: remove unused debounce utility from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-// Simple debounce utility
-export function debounce<T extends (...args: any[]) => void>(
-  fn: T,
-  delay: number,
-) {
-  let timer: ReturnType<typeof setTimeout>;
-  return (...args: Parameters<T>) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
-  };
-}
