8f773646d743a536d72932ed7842b08e839530b2
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2026-05-21 10:36:19 +0300

    feat(config): add remote pattern for res.cloudinary.com in image configuration

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "xubohuah.github.io",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
