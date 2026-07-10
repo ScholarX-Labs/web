be894134093f4bf3cd87352c4d738ce6177d3ad7
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2026-07-10 12:36:29 +0300

    style(ui): add unused useEffect import

--- src/components/ui/canvas-reveal-effect.tsx
+++ src/components/ui/canvas-reveal-effect.tsx
@@ -17,7 +17,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import * as THREE from "three";
 
 export const CanvasRevealEffect = ({
